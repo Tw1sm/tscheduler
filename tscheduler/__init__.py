2d36b003ab2e96cc469b085c3311e595cb14d192
Matt Creel <37981031+Tw1sm@users.noreply.github.com>
2025-02-10 20:26:38 -0500

    typo

--- tscheduler/__init__.py
+++ tscheduler/__init__.py
@@ -3,4 +3,4 @@ from tscheduler.logger import OBJ_EXTRA_FMT, logging, ColorScheme, console
 __version__ = '0.1.0'
 
 def banner():
-    logging.info(f'tschduler [cyan]v{__version__}[/]', extra=OBJ_EXTRA_FMT)
+    logging.info(f'tscheduler [cyan]v{__version__}[/]', extra=OBJ_EXTRA_FMT)
